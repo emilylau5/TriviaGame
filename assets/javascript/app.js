bdec53459ccaee25e8d5b82e10b9e157d83ff878
Emily Lau <emilylau5@gmail.com>
2017-09-28 18:31:02 -0700

    final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,24 +11,12 @@
 // settimeout for correct answer display
 // next question
 // add id to button tag to show correct or incorrect
-var timer = 10;
+var timer = 120;
 var intervalID;
 var correctAnswers = 0;
 var incorrectAnswers = 0;
-var unanswered = 4;
-// var questions = {
-//   numberOne: "What is the name of the baboon in The Lion King?", 
-//   numberTwo: "Who trained Hercules to go from zero to hero?",
-//   numberThree: "Who is Woody's owner in The Toy Story?",
-//   numberFour: "What is Sleeping Beauty's real name?",
-//   };
-// var answers = {
-//   numberOne: ["Rakfiki", "Tafiki", "Zafiki","Nafiki"],
-//   numberTwo: ["Zeus", "Zack", "Phil", "Toby"],
-//   numberThree: ["Eric", "Andy", "Tommy", "Billy"],
-//   numberFour: ["Aurora", "Crystal", "Calypso", "Ariana"],
+var unanswered = 10;
 
-// }
 function reset() {
   $("#clear").hide()
   $("#start").html("<button type=button>" + "Start" + "</buttons>");
@@ -45,11 +33,11 @@ $("#start").hide();
 function displayScore() {
   $("#clear").html("Correct Answers: " + correctAnswers + "<br>" + "Incorrect Answers: " + incorrectAnswers + "<br>" +
     "Unanswered Questions: " + (unanswered - (correctAnswers + incorrectAnswers)));
-  // var resetBtn = $("<button>");
-  $("#buttons").append("<button type=button>" + "Reset" + "</buttons>");
-  $("#buttons").on("click", function() {
-    reset();
-  })
+  // sorry, i tried creating a reset button but the button wouldnt append
+  // $("#buttons").append("<button type=button>" + "Reset" + "</buttons>");
+  // $("#buttons").on("click", function() {
+  //   reset();
+  // })
 }
 function testTimer() {
   intervalID = setInterval(decrement, 1000)
